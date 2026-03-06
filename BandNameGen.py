86e6ce6b917f89139f516b7fe7f398da7b89e909
Dave <75653950+davegirton@users.noreply.github.com>
2026-03-06 11:30:05 -0600

    Add files via upload

---
+++ BandNameGen.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name generator.")
+city = input("What city did you grow up in?\n")
+petname = input("What is your pets name?\n")
+print("Here is a possible name for your band, " + city + " " + petname +".")
---
+++ EverOddNumber.py
@@ -0,0 +1,6 @@
+user_input = int(input("Enter a whole number and find out if it is Even or Odd."))
+result = user_input % 2
+if result == 0:
+    print("Even")
+else:
+    print("Odd")
